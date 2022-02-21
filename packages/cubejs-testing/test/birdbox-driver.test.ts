095096b21b14648adc1fe666259ff6636d6d15d1
buntarb <buntarb@gmail.com>
2022-02-21 13:39:40 +0200

    filters test cases added to bigquery and athena snapshots

--- packages/cubejs-testing/test/birdbox-driver.test.ts
+++ packages/cubejs-testing/test/birdbox-driver.test.ts
@@ -1,6 +1,6 @@
 import yargs from 'yargs/yargs';
 // eslint-disable-next-line import/no-extraneous-dependencies
-import cubejs, { CubejsApi } from '@cubejs-client/core';
+import cubejs, { Query, CubejsApi } from '@cubejs-client/core';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { afterAll, beforeAll, expect, jest } from '@jest/globals';
 import { BirdBox, startBirdBoxFromCli, startBirdBoxFromContainer } from '../src';
@@ -109,6 +109,291 @@ describe(name, () => {
     );
     // ../.. to move out of dist/test directory
     // @ts-ignore
-    expect(response.rawData()).toMatchSpecificSnapshot(`../../test/__snapshots__/${name}.query.snapshot`);
+    expect(response.rawData()).toMatchSpecificSnapshot(
+      `../../test/__snapshots__/${name}.query.snapshot`
+    );
+  });
+
+  describe('filters', () => {
+    type QueryTestOptions = {
+      name: string;
+      ws?: true,
+    };
+
+    const containsAsserts: [options: QueryTestOptions, query: Query][] = [
+      [
+        {
+          name: '#1 Orders.status.contains: ["e"]',
+        },
+        {
+          measures: [
+            'Orders.count'
+          ],
+          filters: [
+            {
+              member: 'Orders.status',
+              operator: 'contains',
+              values: ['e'],
+            },
+          ],
+        },
+      ], [
+        {
+          name: '#2 Orders.status.contains: ["es"]',
+        },
+        {
+          measures: [
+            'Orders.count'
+          ],
+          filters: [
+            {
+              member: 'Orders.status',
+              operator: 'contains',
+              values: ['es'],
+            },
+          ],
+        },
+      ], [
+        {
+          name: '#3 Orders.status.contains: ["es", "w"]',
+        },
+        {
+          measures: [
+            'Orders.count'
+          ],
+          filters: [
+            {
+              member: 'Orders.status',
+              operator: 'contains',
+              values: ['es', 'w'],
+            },
+          ],
+        },
+      ], [
+        {
+          name: '#3 Orders.status.contains: ["a"]',
+        },
+        {
+          measures: [
+            'Orders.count'
+          ],
+          filters: [
+            {
+              member: 'Orders.status',
+              operator: 'contains',
+              values: ['a'],
+            },
+          ],
+        },
+      ],
+    ];
+    const startsWithAsserts: [options: QueryTestOptions, query: Query][] = [
+      [
+        {
+          name: '#1 Orders.status.startsWith: ["a"]',
+        },
+        {
+          measures: [
+            'Orders.count'
+          ],
+          filters: [
+            {
+              member: 'Orders.status',
+              operator: 'startsWith',
+              values: ['a'],
+            },
+          ],
+        },
+      ], [
+        {
+          name: '#2 Orders.status.startsWith: ["n"]',
+        },
+        {
+          measures: [
+            'Orders.count'
+          ],
+          filters: [
+            {
+              member: 'Orders.status',
+              operator: 'startsWith',
+              values: ['n'],
+            },
+          ],
+        },
+      ], [
+        {
+          name: '#3 Orders.status.startsWith: ["p"]',
+        },
+        {
+          measures: [
+            'Orders.count'
+          ],
+          filters: [
+            {
+              member: 'Orders.status',
+              operator: 'startsWith',
+              values: ['p'],
+            },
+          ],
+        },
+      ], [
+        {
+          name: '#4 Orders.status.startsWith: ["sh"]',
+        },
+        {
+          measures: [
+            'Orders.count'
+          ],
+          filters: [
+            {
+              member: 'Orders.status',
+              operator: 'startsWith',
+              values: ['sh'],
+            },
+          ],
+        },
+      ], [
+        {
+          name: '#5 Orders.status.startsWith: ["n", "p", "s"]',
+        },
+        {
+          measures: [
+            'Orders.count'
+          ],
+          filters: [
+            {
+              member: 'Orders.status',
+              operator: 'startsWith',
+              values: ['n', 'p', 's'],
+            },
+          ],
+        },
+      ],
+    ];
+    const endsWithAsserts: [options: QueryTestOptions, query: Query][] = [
+      [
+        {
+          name: '#1 Orders.status.endsWith: ["a"]',
+        },
+        {
+          measures: [
+            'Orders.count'
+          ],
+          filters: [
+            {
+              member: 'Orders.status',
+              operator: 'endsWith',
+              values: ['a'],
+            },
+          ],
+        },
+      ], [
+        {
+          name: '#2 Orders.status.endsWith: ["w"]',
+        },
+        {
+          measures: [
+            'Orders.count'
+          ],
+          filters: [
+            {
+              member: 'Orders.status',
+              operator: 'endsWith',
+              values: ['w'],
+            },
+          ],
+        },
+      ], [
+        {
+          name: '#3 Orders.status.endsWith: ["sed"]',
+        },
+        {
+          measures: [
+            'Orders.count'
+          ],
+          filters: [
+            {
+              member: 'Orders.status',
+              operator: 'endsWith',
+              values: ['sed'],
+            },
+          ],
+        },
+      ], [
+        {
+          name: '#4 Orders.status.endsWith: ["ped"]',
+        },
+        {
+          measures: [
+            'Orders.count'
+          ],
+          filters: [
+            {
+              member: 'Orders.status',
+              operator: 'endsWith',
+              values: ['ped'],
+            },
+          ],
+        },
+      ], [
+        {
+          name: '#4 Orders.status.endsWith: ["w", "sed", "ped"]',
+        },
+        {
+          measures: [
+            'Orders.count'
+          ],
+          filters: [
+            {
+              member: 'Orders.status',
+              operator: 'endsWith',
+              values: ['w', 'sed', 'ped'],
+            },
+          ],
+        },
+      ],
+    ];
+
+    describe('contains', () => {
+      // eslint-disable-next-line no-restricted-syntax
+      for (const [options, query] of containsAsserts) {
+        // eslint-disable-next-line no-loop-func
+        it(`${options.name}`, async () => {
+          const response = await httpClient.load(query);
+          // @ts-ignore
+          expect(response.rawData()).toMatchSpecificSnapshot(
+            `../../test/__snapshots__/${name}.query.snapshot`
+          );
+        });
+      }
+    });
+
+    describe('startsWith', () => {
+      // eslint-disable-next-line no-restricted-syntax
+      for (const [options, query] of startsWithAsserts) {
+        // eslint-disable-next-line no-loop-func
+        it(`${options.name}`, async () => {
+          const response = await httpClient.load(query);
+          // @ts-ignore
+          expect(response.rawData()).toMatchSpecificSnapshot(
+            `../../test/__snapshots__/${name}.query.snapshot`
+          );
+        });
+      }
+    });
+
+    describe('endsWith', () => {
+      // eslint-disable-next-line no-restricted-syntax
+      for (const [options, query] of endsWithAsserts) {
+        // eslint-disable-next-line no-loop-func
+        it(`${options.name}`, async () => {
+          const response = await httpClient.load(query);
+          // @ts-ignore
+          expect(response.rawData()).toMatchSpecificSnapshot(
+            `../../test/__snapshots__/${name}.query.snapshot`
+          );
+        });
+      }
+    });
   });
 });
